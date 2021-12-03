5c8a44d4461a8844af3b25a9f1e018cc4e9e02db
Lev <lyoha.bel2020@gmail.com>
2021-12-03 13:40:49 +0300

    feature/BB-10-11 sth changed

--- src/components/molecules/Footer/index.js
+++ src/components/molecules/Footer/index.js
@@ -2,12 +2,10 @@ import React from "react";
 import * as styles from "./styles.module.scss";
 
 function Footer() {
-  const date = new Date();
-  const year = date.getFullYear();
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
-        <div className={styles.footerText}>{year}</div>
+        <div className={styles.footerText}>{new Date().getFullYear()}</div>
         <div className={styles.footerText}>@autor</div>
       </div>
     </footer>
