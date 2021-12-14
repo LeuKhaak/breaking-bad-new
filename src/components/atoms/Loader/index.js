165702f72c869d4940e869b5fa625cd5b49f508b
Lev <lyoha.bel2020@gmail.com>
2021-12-14 15:24:08 +0300

    feature/BB-18 testing error-2

--- src/components/atoms/Loader/index.js
+++ src/components/atoms/Loader/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import * as styles from "./styles.module.scss";
-import loader from "src/assets/images/loader.gif";
+import loader from "../../../assets/images/loader.gif";
 
 function Loader() {
   return (
